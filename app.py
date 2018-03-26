dc0b2296c279fcc6e26795768fd327319f135c0c
Andrea Lowe <andrea.s.lowe@gmail.com>
2018-03-26 01:08:29 +0000

    added plot

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import requests
 import simplejson as json
 import pandas as pd
@@ -7,27 +6,28 @@ import datetime as dt
 from bokeh.plotting import figure, output_file, show
 
 #get ticker symbol, set date range, pull data from Quandl, and put in dataframe
-#def get_data(ticker):
-#	
-#	current_date = dt.datetime.now().date().strftime("%Y-%m-%d")
-#	begin_date = dt.datetime.now().date() + dt.timedelta(-30)
-#	begin_date = begin_date.strftime("%Y-%m-%d")
-#
-#	#import data
-#	myURL = 'https://www.quandl.com/api/v3/datasets/WIKI/' + ticker + 
-#	'/data.json?api_key=Ny7LjkeNwh1aFxjHewmJ&start_date=' + begin_date + '&end_date=' + current_date
-#
-#	r = requests.get(myURL)
-#	rawdata = r.json()['dataset_data']
-#	stock_df = df.DataFrame(rawdata['data'], columns = rawdata['column_names'])
-#	return stock_df
+def get_data(ticker):
+	
+	current_date = dt.datetime.now().date().strftime("%Y-%m-%d")
+	begin_date = dt.datetime.now().date() + dt.timedelta(-30)
+	begin_date = begin_date.strftime("%Y-%m-%d")
+
+	#import data
+	myURL = 'https://www.quandl.com/api/v3/datasets/WIKI/' + ticker + 
+	'/data.json?api_key=Ny7LjkeNwh1aFxjHewmJ&start_date=' + begin_date + '&end_date=' + current_date
+
+	r = requests.get(myURL)
+	rawdata = r.json()['dataset_data']
+	stock_df = df.DataFrame(rawdata['data'], columns = rawdata['column_names'])
+	return stock_df
 
 #graph data
-#def plot_data(stock_df, data_sel):
-#	p = figure(title="Quandl Stock Prices", x_axis_label='Date', y_axis_label='Price')
-#	if data_sel == 'closing':	
-#		p.line(stock_df['Date'], stock_df['Close'], legend="Selection", line_width=2)
-
+def plot_data(stock_df): #, data_sel
+	p = figure(title="Quandl Stock Prices", x_axis_label='Date', y_axis_label='Price')
+	#if data_sel == 'closing':	
+	p.line(stock_df['Date'], stock_df['Close'], legend="Selection", line_width=2)
+	script, div = components(p)
+	return script, div
 
 
 
@@ -56,8 +56,12 @@ def index():
  #code to use text box to get stock ticker info and check boxes for user input
 @app.route('/', methods=['POST'])
 def my_form_post():
-    tickStr = request.form['tickerText']
-	app.vars['ticker'] = tickStr.upper()
+    tick_str = request.form['tickerText']
+	app.vars['ticker'] = tick_str.upper()
+
+	stock_df = get_data(apps.vars['ticker'])
+	script, div = plot_data(stock_df)
+	return render_template('plot.html', script=script, div=div)
 
 
 
