0250e86c3f64a66a6ca2bc257c44c7c73f269958
Andrea Lowe <andrea.s.lowe@gmail.com>
2018-03-26 00:54:46 +0000

    v4

--- app.py
+++ app.py
@@ -7,26 +7,26 @@ import datetime as dt
 from bokeh.plotting import figure, output_file, show
 
 #get ticker symbol, set date range, pull data from Quandl, and put in dataframe
-def get_data(ticker):
-	
-	current_date = dt.datetime.now().date().strftime("%Y-%m-%d")
-	begin_date = dt.datetime.now().date() + dt.timedelta(-30)
-	begin_date = begin_date.strftime("%Y-%m-%d")
-
-	#import data
-	myURL = 'https://www.quandl.com/api/v3/datasets/WIKI/' + ticker + 
-	'/data.json?api_key=Ny7LjkeNwh1aFxjHewmJ&start_date=' + begin_date + '&end_date=' + current_date
-
-	r = requests.get(myURL)
-	rawdata = r.json()['dataset_data']
-	stock_df = df.DataFrame(rawdata['data'], columns = rawdata['column_names'])
-	return stock_df
+#def get_data(ticker):
+#	
+#	current_date = dt.datetime.now().date().strftime("%Y-%m-%d")
+#	begin_date = dt.datetime.now().date() + dt.timedelta(-30)
+#	begin_date = begin_date.strftime("%Y-%m-%d")
+#
+#	#import data
+#	myURL = 'https://www.quandl.com/api/v3/datasets/WIKI/' + ticker + 
+#	'/data.json?api_key=Ny7LjkeNwh1aFxjHewmJ&start_date=' + begin_date + '&end_date=' + current_date
+#
+#	r = requests.get(myURL)
+#	rawdata = r.json()['dataset_data']
+#	stock_df = df.DataFrame(rawdata['data'], columns = rawdata['column_names'])
+#	return stock_df
 
 #graph data
-def plot_data(stock_df, data_sel):
-	p = figure(title="Quandl Stock Prices", x_axis_label='Date', y_axis_label='Price')
-	if data_sel == 'closing':	
-		p.line(stock_df['Date'], stock_df['Close'], legend="Selection", line_width=2)
+#def plot_data(stock_df, data_sel):
+#	p = figure(title="Quandl Stock Prices", x_axis_label='Date', y_axis_label='Price')
+#	if data_sel == 'closing':	
+#		p.line(stock_df['Date'], stock_df['Close'], legend="Selection", line_width=2)
 
 
 
@@ -57,10 +57,8 @@ def index():
 @app.route('/', methods=['POST'])
 def my_form_post():
     tickStr = request.form['tickerText']
-	reqList = request.form['priceCheck'] # checkboxes
-
 	app.vars['ticker'] = tickStr.upper()
-	app.vars['priceReqs'] = reqList
+
 
 
 
