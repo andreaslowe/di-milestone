659fd32cf1b2803ee3504f1ea5d3977602ffb2b8
Andrea Lowe <andrea.s.lowe@gmail.com>
2018-03-26 03:20:08 +0000

    more flask

--- app.py
+++ app.py
@@ -24,10 +24,16 @@ def get_data(ticker):
 	return stock_df
 
 #graph data
-def plot_data(stock_df): #, data_sel
+def plot_data(stock_df, data_sel): 
     p = figure(title="Quandl Stock Prices", x_axis_label='Date', y_axis_label='Price')
-	#if data_sel == 'closing':	
-	p.line(stock_df['Date'], stock_df['Close'], legend="Closing Price", line_width=2)
+	if 'closing' in data_sel:	
+	   p.line(stock_df['Date'], stock_df['Close'], legend="Closing Price", line_width=2)
+	if 'opening' in data_sel:	
+	   p.line(stock_df['Date'], stock_df['Open'], legend="Opening Price", line_width=2, line_color = "red")
+	if 'adj_opening' in data_sel:	
+	   p.line(stock_df['Date'], stock_df['Adj. Open'], legend="Adjusted Opening Price", line_width=2, line_color = "black")
+    if 'adj_opening' in data_sel:	
+	   p.line(stock_df['Date'], stock_df['Adj. Close'], legend="Adjusted Closing Price", line_width=2, line_color = "purple")
 	script, div = components(p)
 	return script, div
 
@@ -67,7 +73,7 @@ def my_form_post():
 
 
 if __name__ == '__main__':
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host='0.0.0.0', port=port)
-	app.run(port=33507)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+	#app.run(port=33507)
   
